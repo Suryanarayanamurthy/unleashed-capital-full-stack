6de44f8ea1ce365c5cf93b0f74619a0bc2396755
Zurya <zurya@somerandomguy.xyz>
2020-11-28 17:23:12 +0100

    working proto of jwt

---
+++ backend/auth-module/config.js
@@ -0,0 +1,6 @@
+mongouri: 
+
+module.exports = {
+    mongo_connection_str: "mongodb+srv://unleased_devuser0:21fBdwNyS2nup1pG@cluster0.3pu3d.mongodb.net/users?retryWrites=true&w=majority",
+  };
+  
